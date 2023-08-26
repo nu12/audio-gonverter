28469a6a06feca6ea5f88ce8311de77c855b9ed7
nu12 <34694287+nu12@users.noreply.github.com>
2023-08-26 19:57:27 -0400

    feat: file upload

---
+++ internal/database/mock.go
@@ -0,0 +1,34 @@
+package database
+
+import "github.com/nu12/audio-gonverter/internal/model"
+
+type MockDB struct{}
+
+func (*MockDB) Save(*model.User) error {
+	return nil
+}
+
+func (*MockDB) Load(id string) *model.User {
+	return &model.User{
+		UUID:         id,
+		IsUploading:  false,
+		IsConverting: false,
+		Files: []model.File{
+			{
+				OriginalName:  "MockFile1.mp3",
+				ConvertedName: "MockFile1.ogg",
+				OriginalSize:  4011,
+				ConvertedSize: 5512,
+				OriginalId:    "xxx-xxx-xxx-xxx",
+				ConvertedId:   "yyy-yyy-yyy-yyy",
+				IsConverted:   true,
+			},
+			{
+				OriginalName: "MockFile1.mp3",
+				OriginalSize: 8752,
+				OriginalId:   "zzz-zzz-zzz-zzz",
+				IsConverted:  false,
+			},
+		},
+	}
+}
---
+++ internal/repository/database.go
@@ -0,0 +1,8 @@
+package repository
+
+import "github.com/nu12/audio-gonverter/internal/model"
+
+type DatabaseRepository interface {
+	Save(*model.User) error
+	Load(string) *model.User
+}
